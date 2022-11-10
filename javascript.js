ad289148d8466047826a49686cb33de22649b699
Rob Hamburg <robhamburg89@gmail.com>
2022-11-10 15:35:03 +0000

    set up hover and rub effects

--- javascript.js
+++ javascript.js
@@ -3,8 +3,31 @@ const container = document.querySelector('#container')
 
 
 for (let i = 0; i < 256; i++) {
-   let square = document.createElement('div')
+   var square = document.createElement('div')
     square.classList.add('square')
-    container.appendChild(square)
+    container.appendChild(square)                //populates the grid
 }
+
+
+
+    let colour = document.querySelectorAll('.square') //need queryselectorall because queryselector only gets the first one returned
+    
+   
+
+    for (let z = 0; z < colour.length; z++) {
+                                                               //upon hover each cell changes colour
+        colour[z].addEventListener('mouseover', () => {
+            colour[z].classList.add('square-filled')
+            colour[z].classList.remove('square')
+        })
+
+        colour[z].addEventListener('click', () => {            //upon click each cell reverts back to filled
+            colour[z].classList.add('square')
+            colour[z].classList.remove('square-filled')
+        })
+                
+        }
+
+
+   
 
