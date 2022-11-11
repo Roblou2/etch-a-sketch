35d15856c535dee8db012404c88f5b67197e8d3c
Rob Hamburg <robhamburg89@gmail.com>
2022-11-11 13:53:11 +0000

    Make erase feature

--- javascript.js
+++ javascript.js
@@ -1,33 +1,79 @@
+const container = document.querySelector('#container') //add the grid to memory to grab in the for loop below
 
-const container = document.querySelector('#container')
+let penOn = false;
+container.onmousedown = function () {      //configure the pen on/off switch to work on holding the mouse down 
+    penOn = true;
+   
+}
+container.onmouseup = function () {
+    penOn = false;
+  
+}
 
+//the foor loop which creates coloured trail
 
 for (let i = 0; i < 256; i++) {
-   var square = document.createElement('div')
-    square.classList.add('square')
-    container.appendChild(square)                //populates the grid
+ 
+    var square = document.createElement('div') //create divs for grid
+    square.classList.add('square')    //add the default square class
+
+    container.appendChild(square)     //add divs to grid
+    square.addEventListener('mouseover', function (e) {  //upon mouse over and penOn switch set to 'on', switch the class of the cell
+        if(penOn === true) {
+     
+    e.target.classList.add('square-filled')
+    e.target.classList.remove('square')
+        }
+    })
+   square.addEventListener('mousedown', function (e) {
+    if(penOn === true) {
+      
+        e.target.classList.add('square-filled')      //upon mouse down and penOn switch set to 'on', change the class of the cell
+        e.target.classList.remove('square')
+    }
+})
+   
 }
 
 
 
-    let colour = document.querySelectorAll('.square') //need queryselectorall because queryselector only gets the first one returned
-    
-   
 
-    for (let z = 0; z < colour.length; z++) {
-                                                               //upon hover each cell changes colour
-        colour[z].addEventListener('mouseover', () => {
-            colour[z].classList.add('square-filled')
-            colour[z].classList.remove('square')
-        })
+//erase button - makes new grid and calls same for loop function below
+const rubber = document.querySelector('#rubber')
 
-        colour[z].addEventListener('click', () => {            //upon click each cell reverts back to filled
-            colour[z].classList.add('square')
-            colour[z].classList.remove('square-filled')
-        })
-                
+rubber.addEventListener('click', reset)
+
+function reset() {
+ while(container.hasChildNodes()) {         //removes current divs under grid container
+    container.removeChild(container.firstChild)
+ }
+                      //here we make the fresh divs again and call same draw functions as above
+  for (let z = 0; z < 256; z++) {
+    var square = document.createElement('div') 
+    square.classList.add('square')    
+    container.appendChild(square)     
+
+    container.appendChild(square)
+    square.addEventListener('mouseover', function (e) {  
+        if(penOn === true) {
+     
+    e.target.classList.add('square-filled')
+    e.target.classList.remove('square')
         }
+    })
+   square.addEventListener('mousedown', function (e) {
+    if(penOn === true) {
+      
+        e.target.classList.add('square-filled')      
+        e.target.classList.remove('square')
+    }
+})
+
+  }
+  
+}
+ 
 
 
-   
+ 
 
