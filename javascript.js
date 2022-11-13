3276b32ef37565586733f4d799e284d699adf8d2
Rob Hamburg <robhamburg89@gmail.com>
2022-11-13 13:38:27 +0000

    setup colour selector and get it working

--- javascript.js
+++ javascript.js
@@ -22,83 +22,70 @@ reset()
 
 })
 
-//configure the pen on/off switch to work on holding the mouse down 
+//configure the pen on/off switch for the grid container children
 let penOn = false;
 container.onmousedown = function () {      
     penOn = true;
-   
+  
 }
 container.onmouseup = function () {
     penOn = false;
   
 }
 
-//set green colour switch
-let green = false;
+//set colour output
 
-document.querySelector('.green').addEventListener('click', () => {
-    green = true;
-    red = false;
+const defaultColour = '#333333'
+
+const colour = document.getElementById("colorWell")
+let colourSelected = defaultColour
+
+function setColour (newColour) {  //newColour grabbed from inline function below
+colourSelected = newColour
 }
-)
 
-//set red colour switch 
+colour.oninput = (e) => setColour(e.target.value) //this gets the value of the colour selected and feeds it back into the setColour function
 
-let red = false;
-document.querySelector('.red').addEventListener('click', () => {
-    red = true;
-    green = false;
-})
+
+
 
 //the foor loop to load the default gridSize (small) upon page load
 function start () {
 for (let z = 0; z < gridSize; z++) {
-    var square = document.createElement('div') 
-    square.classList.add('square')    
+    let square = document.createElement('div') 
+    square.classList.add('square')  
+
     container.appendChild(square)     
 
-    container.appendChild(square)
-    square.addEventListener('mouseover', function (e) {  
-        if(penOn === true && green === true) {   //set colour green with switch true
+    square.addEventListener('mousedown', function (e) {
+        if(penOn === true && colourButtonOn === true) {                
          
-    e.target.setAttribute('id', 'square-filled-green') //ids higher specificity means will colour over if square is red
-   
+       
+           
+            e.target.style.backgroundColor = colourSelected
+           
+        }
+
+    })
+
+    square.addEventListener('mouseover', function (e) {  
+        if(penOn === true) {   
+        
+   e.target.style.backgroundColor = colourSelected
 
         }
+
     })
-   square.addEventListener('mousedown', function (e) {
-    if(penOn === true && green === true) {                //set green pink with switch true
-     
-        e.target.setAttribute('id', 'square-filled-green')    
-     
-      
-    }
-})
-square.addEventListener('mouseover', function (e) {  
-    if(penOn === true && red === true) {   //set colour red with switch true
-     
-e.target.setAttribute('id', 'square-filled-red')
 
-
-    }
-})
-square.addEventListener('mousedown', function (e) {
-if(penOn === true && red === true) {                //set colour red with switch true
- 
-    e.target.setAttribute('id', 'square-filled-red')  
-   
-  
-}
-})
   }
 }
 
 
 
 //erase button and fresh grid for big and small buttons
-const rubber = document.querySelector('.rubber')
+const clear = document.querySelector('.clear')
 
-rubber.addEventListener('click', reset)
+clear.addEventListener('click', reset)
 
 function reset() {
  while(container.hasChildNodes()) {         //removes current divs under grid container
@@ -107,40 +94,25 @@ function reset() {
  }
  for (let z = 0; z < gridSize; z++) {
   
-    var square = document.createElement('div') 
+    let square = document.createElement('div') 
     square.classList.add('square')    
+
     container.appendChild(square)     
 
-    container.appendChild(square)
     square.addEventListener('mouseover', function (e) {  
-        if(penOn === true && green === true) {   //set colour greebn with switch true
+        if(penOn === true) {   
          
-            e.target.setAttribute('id', 'square-filled-green')  
+            e.target.style.backgroundColor = colourSelected //grabbed from the setColour function above
   
         }
     })
    square.addEventListener('mousedown', function (e) {
-    if(penOn === true && green === true) {                //set colour green with switch true
+    if(penOn === true) {               
      
-        e.target.setAttribute('id', 'square-filled-green')   
+        e.target.style.backgroundColor = colourSelected
     
        
     }
-})
-square.addEventListener('mouseover', function (e) {  
-    if(penOn === true && red === true) {   //set colour red with switch true
-     
-        e.target.setAttribute('id', 'square-filled-red')  
-
-    }
-})
-square.addEventListener('mousedown', function (e) {
-if(penOn === true && red === true) {                //set colour red with switch true
- 
-    e.target.setAttribute('id', 'square-filled-red')     
-
-    
-}
 })
 
   }
